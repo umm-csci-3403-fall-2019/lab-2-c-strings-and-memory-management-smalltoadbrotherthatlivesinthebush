10a03e3a646cb57c8e47e2f7d2161252ea310f9c
robe1606 <robe1606@morris.umn.edu>
2019-09-24 11:40:03 -0500

    Create disemvowel file that passes tests

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,22 @@
 #include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 
 #include "disemvowel.h"
 
 char *disemvowel(char *str) {
-  return (char*) "";
+  int len, i;
+  char *result;
+
+  len=strlen(str);
+  result = (char*) calloc(len+1, sizeof(char));
+  int index = 0;
+  for (i=0; i<len; i++){
+	  if((str[i] != 'a') && (str[i] != 'e') && (str[i] != 'i') && (str[i] != 'o') && (str[i] != 'u') && (str[i] != 'A') && (str[i] != 'E') && (str[i] != 'I') && (str[i] != 'O') && (str[i] != 'U')) {
+		  result[index] = str[i];
+		  index++;
+	  }
+  }
+  result[len] = '\0';
+  return result;
 }
